87b299231e406baa739694ec2417607fc419a142
Henrique Boaventura <hboaventura@gmail.com>
2010-04-27 23:36:54 -0300

    alterada a ordenacao do menu

--- lib/model/doctrine/CategoryTable.class.php
+++ lib/model/doctrine/CategoryTable.class.php
@@ -7,7 +7,8 @@ class CategoryTable extends Doctrine_Table
         $q = $this->createQuery()
            ->from('Category c')
            ->innerJoin('c.Pages p')
-           ->where("p.slug NOT IN ('sobre-a-absw','sobre-o-site')");
+           ->where("p.slug NOT IN ('sobre-a-absw','sobre-o-site')")
+           ->orderBy('c.id, p.title');
 
         return $q->execute();
     }
