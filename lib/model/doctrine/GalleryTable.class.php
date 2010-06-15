07a61317b465ad726b1024a66431af2ce864c4f5
Henrique <hboaventura@gmail.com>
2010-06-15 17:39:12 -0300

    adicionado o slug novamente a consulta

--- lib/model/doctrine/GalleryTable.class.php
+++ lib/model/doctrine/GalleryTable.class.php
@@ -12,7 +12,7 @@ class GalleryTable extends Doctrine_Table
     public function listGalleriesByDate()
     {
         $q = $this->createQuery()
-           ->select('g.id, g.created_at, g.title, (SELECT p.path FROM Photo p WHERE p.gallery_id = g.id LIMIT 1) AS photo')
+           ->select('g.id, g.created_at, g.title, g.slug, (SELECT p.path FROM Photo p WHERE p.gallery_id = g.id LIMIT 1) AS photo')
            ->from('Gallery g')
            ->orderBy('g.created_at DESC');
 
