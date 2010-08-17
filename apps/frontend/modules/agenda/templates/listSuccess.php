b9b848e774ae00fff1bc0f316f0db614793c9d3e
Henrique <hboaventura@gmail.com>
2010-08-17 14:05:14 -0300

    corrigido o formato da data

--- apps/frontend/modules/agenda/templates/listSuccess.php
+++ apps/frontend/modules/agenda/templates/listSuccess.php
@@ -7,7 +7,7 @@
         foreach($events as $event){
     ?>
     <dt><?php echo format_date($event->date,'dd/MM/y'); ?> - <?php echo $event->title; ?></dt>
-    <dd>Hora: <?php echo format_date($event->time,'hh:mm') ?></dd>
+    <dd>Hora: <?php echo format_date($event->time,'HH:mm') ?></dd>
     <dd>Local: <?php echo $event->place ?></dd>
     <dd class="info">Informações: <?php echo $event->info ?></dd>
     <?php
